b2e21e81d0e8f81acdd72a23f7f2923c786c18fe
Nnamdi <nnamdiamadi391@gmail.com>
2025-12-19 01:57:46 -0800

    Function

---
+++ testData.js
@@ -0,0 +1,22 @@
+//skills data
+const toolsData =[
+   {
+    "name": "HTML",
+    "img_alt": "HTML5",
+    "img_src":"" ,
+   },
+   {
+    "name": "CSS",
+    "img_alt": "CSS3",
+    "img_src": "",
+   },
+   {
+    "name": "JS",
+   }
+   {
+    "name": "TAILWINDCSS",
+    "img_alt": "TAILWINDCSS",
+    "img_src": "",
+   }
+]
+
